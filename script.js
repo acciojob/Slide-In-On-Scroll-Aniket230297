1435cd58a2a8f9680429416165e1a16e31f44feb
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-02-21 20:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your JS code here.
+function addSlide() {
+  scrolledImages.forEach((img) => {
+    var scrolledHeight = window.scrollY + window.innerHeight - img.height / 2;
+    var imageBottomHeight = img.offsetTop + img.height;
+    var isImageHalfScrolled = scrolledHeight > img.offsetTop;
+
+    var isImageFullyScrolled = window.scrollY > imageBottomHeight;
+
+    if (isImageHalfScrolled && !isImageFullyScrolled) {
+      img.classList.add("active");
+    } else {
+      img.classList.remove("active");
+    }
+  });
+}
